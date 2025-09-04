b21d199951bf3072610a2e7e968e2c966ff91a55
theblackmamba08 <fossocabrel08@gmail.com>
2025-09-04 07:08:39 +0100

    implementation des routes du serveur

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const phonebook = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+// app.use(morgan('tiny'));
+morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '');
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+// const requestLogger = (request, response, next) => {
+//     console.log('Method:', request.method);
+//     console.log('Path:  ', request.path);
+//     console.log('Body:  ', request.body);
+//     console.log('---');
+//     next();
+// }
+// app.use(requestLogger);
+
+
+app.get('/info', (request, response) => {
+    const date = new Date();
+    response.send(`<p>Phonebook has info for ${phonebook.length} people</p><p>${date}</p>`);
+});
+
+app.get('/api/persons', (request, response) => {
+    response.json(phonebook);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = phonebook.find(person => person.id === id);
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+});
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+            error: 'name or number is missing' 
+        });
+    }
+    if (phonebook.find(person => person.name === body.name)) {
+        return response.status(400).json({ 
+            error: 'name must be unique' 
+        });
+    }
+    const person = {
+        id: Math.floor(Math.random() * 10000),
+        name: body.name,
+        number: body.number,
+    };
+    phonebook.push(person);
+    response.json(person);
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = phonebook.filter(person => person.id !== id);
+    response.status(204).end();
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
