d1774c09e4f147e3da0e9a40cef120c606d5d3c7
theblackmamba08 <fossocabrel08@gmail.com>
2025-09-04 07:50:37 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.static('dist'));
 
